3951a8f120440adff9ea3d6a3fe3117461db5498
669344682 <hellotianyuan@gamil.com>
2023-05-15 19:39:25 +1000

    main

---
+++ server.lua
@@ -0,0 +1,39 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
+local players = {}
+
+RegisterNetEvent('qb-nametag:server:GetPlayers', function()
+    local src = source
+    TriggerClientEvent('qb-nametag:client:ShowNametag', src, players)
+end)
+
+
+CreateThread(function()
+    while true do
+        local tempPlayers = {}
+        for _, v in pairs(QBCore.Functions.GetPlayers()) do
+            local targetped = GetPlayerPed(v)
+            local ped = QBCore.Functions.GetPlayer(v)
+
+            tempPlayers[#tempPlayers + 1] = {
+                name = (ped.PlayerData.charinfo.firstname or '') .. ' ' .. (ped.PlayerData.charinfo.lastname or ''), --  .. ' | (' .. (GetPlayerName(v) or '') .. ')',
+                id = v,
+                coords = GetEntityCoords(targetped),
+                cid = ped.PlayerData.charinfo.firstname .. ' ' .. ped.PlayerData.charinfo.lastname,
+                citizenid = ped.PlayerData.citizenid,
+                sources = GetPlayerPed(ped.PlayerData.source),
+                sourceplayer = ped.PlayerData.source,
+
+                job = ped.PlayerData.job or {},
+                joblabel = ped.PlayerData.job.label or 'Civilian',
+            }
+
+        end
+        -- Sort players list by source ID (1,2,3,4,5, etc) --
+        table.sort(tempPlayers, function(a, b)
+            return a.id < b.id
+        end)
+        players = tempPlayers
+        Wait(1500)
+    end
+end)
